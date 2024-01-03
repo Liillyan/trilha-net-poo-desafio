59cc993ddac59ef846aa6d587df568da3c7bce19
Liillyan <lillyanr95@gmail.com>
2024-01-03 19:03:22 -0300

    Resolução do Desafio de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(Numero: "(81)999998888", Modelo: "G11 Plus", Imei: "1234567891011", Memoria: 60 );
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(Numero: "(81)988889999", Modelo: "7 Plus", Imei: "8910111234567", Memoria: 64 );
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
